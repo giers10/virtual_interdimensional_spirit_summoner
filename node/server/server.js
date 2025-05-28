1ba7bc08e23b2038cf5fcdcb18a394da3e3c6e6a
Victor Giers <mail@victorgiers.com>
2025-05-28 05:49:06 +0200

    Comment out console log for server start

--- node/server/server.js
+++ node/server/server.js
@@ -67,5 +67,5 @@ wss.on('connection', (socket) => {
 // --- Server Start ---
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`Server läuft auf http://localhost:${PORT}`);
+  //console.log(`Server läuft auf http://localhost:${PORT}`);
 });
