02d3fa962926a2bc284c826115fa5935c6a4aac7
Victor Giers <mail@victorgiers.com>
2025-05-28 05:42:26 +0200

    auto-git:  [change] node/server/server.js

--- node/server/server.js
+++ node/server/server.js
@@ -57,7 +57,7 @@ function pushSpiritToAllClients() {
 setInterval(() => {
   currentSpiritIndex = Math.floor(Math.random() * spirits.length);
   pushSpiritToAllClients();
-}, 30000);
+}, 20000);
 
 // Beim Start: Ersten Spirit pushen
 setTimeout(pushSpiritToAllClients, 1500);
