f0af5fa9d03dd758ae9fe5760e4b8bed5a6dfd18
Victor Giers <mail@victorgiers.com>
2025-05-28 05:54:53 +0200

    auto-git:  [change] node/server/server.js

--- node/server/server.js
+++ node/server/server.js
@@ -73,9 +73,9 @@ wss.on('connection', (socket) => {
 
   // Starte Timer falls das der erste Client ist
   if (wss.clients.size === 1) {
+    startSpiritTimer();
     // Sende sofort einen Spirit an den neuen Client
     socket.send(JSON.stringify({ type: 'spirit', data: spirits[currentSpiritIndex] }));
-    startSpiritTimer();
   }
 
   // Verbindung verloren: Timer ggf. stoppen
