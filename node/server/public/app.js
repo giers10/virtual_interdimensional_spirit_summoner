631da960d63188baf3e81478c1a60cf5492de889
Victor Giers <mail@victorgiers.com>
2025-05-28 17:49:48 +0200

    Update target FPS for mobile devices in app.js

--- node/server/public/app.js
+++ node/server/public/app.js
@@ -15,7 +15,7 @@ function isMobileDevice() {
 const IS_MOBILE = isMobileDevice();
 const LOW_PERFORMANCE = IS_MOBILE; // ... später aus FPS ableitbar
 
-let targetFps = LOW_PERFORMANCE ? 24 : 60;
+let targetFps = LOW_PERFORMANCE ? 30 : 60;
 let lastAnim = 0;
 
 const container = document.getElementById('viewer');
